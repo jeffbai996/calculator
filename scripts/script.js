690af46147c22d468640ae43732bb25e3e683b80
jeffbai996 <j.bai@wustl.edu>
2022-07-25 18:41:10 -0700

    Added JS for user input to calculator display

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,48 @@
-// get the two numbers specified
-let numOne;
-let numTwo;
+// get number key elements and display element
+let numbers = document.getElementsByClassName('number');
+let display = document.getElementById('display');
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", function() {
+        let buttonValue = this.textContent;
+        if (buttonValue === 'CLEAR') {
+            display.innerHTML = "0";
+        } else {
+            if (display.innerHTML === "0") {
+                display.innerHTML = buttonValue;
+            } else {
+                display.innerHTML = (display.innerHTML + buttonValue).substring(0, 9);
+            }
+        }
+    });
+}
+
 
 // get answer
-const answer = document.getElementById('answer');
-answer.innerText = 0;
+// const answer = document.getElementById('answer');
+// answer.innerText = 0;
 
-function getNums() {
-    numOne = parseInt(document.getElementById('num1').value, 10);
-    numTwo = parseInt(document.getElementById('num2').value, 10);
-}
+// function getNums() {
+//     numOne = parseInt(document.getElementById('num1').value, 10);
+//     numTwo = parseInt(document.getElementById('num2').value, 10);
+// }
 
-function add() {
-    getNums();
-    answer.innerText = numOne + numTwo;
-}
+// function add() {
+//     getNums();
+//     answer.innerText = numOne + numTwo;
+// }
 
-function subtract() {
-    getNums();
-    answer.innerText = numOne - numTwo;
-}
+// function subtract() {
+//     getNums();
+//     answer.innerText = numOne - numTwo;
+// }
 
-function multiply() {
-    getNums();
-    answer.innerText = numOne * numTwo;
-}
+// function multiply() {
+//     getNums();
+//     answer.innerText = numOne * numTwo;
+// }
 
-function divide() {
-    getNums();
-    answer.innerText = numOne / numTwo;
-}
+// function divide() {
+//     getNums();
+//     answer.innerText = numOne / numTwo;
+// }
