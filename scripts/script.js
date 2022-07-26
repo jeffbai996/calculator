148b48efa1e3118571769067624f96cbbdd832ba
jeffbai996 <j.bai@wustl.edu>
2022-07-25 19:33:36 -0700

    Removed old code from testing, added Github link to footer

--- scripts/script.js
+++ scripts/script.js
@@ -12,38 +12,8 @@ for (let i = 0; i < numbers.length; i++) {
             if (display.innerText === "0") {
                 display.innerText = buttonValue;
             } else {
-                display.innerText = (display.innerHTML + buttonValue).substring(0, 10);
+                display.innerText = (display.innerText + buttonValue).substring(0, 10);
             }
         }
     });
 }
-
-
-// get answer
-// const answer = document.getElementById('answer');
-// answer.innerText = 0;
-
-// function getNums() {
-//     numOne = parseInt(document.getElementById('num1').value, 10);
-//     numTwo = parseInt(document.getElementById('num2').value, 10);
-// }
-
-// function add() {
-//     getNums();
-//     answer.innerText = numOne + numTwo;
-// }
-
-// function subtract() {
-//     getNums();
-//     answer.innerText = numOne - numTwo;
-// }
-
-// function multiply() {
-//     getNums();
-//     answer.innerText = numOne * numTwo;
-// }
-
-// function divide() {
-//     getNums();
-//     answer.innerText = numOne / numTwo;
-// }
