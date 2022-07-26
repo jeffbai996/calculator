3198cf8199a27c06d8188136b00ff916f384541a
jeffbai996 <j.bai@wustl.edu>
2022-07-25 22:30:29 -0700

    Moved clear all action into a function

--- scripts/script.js
+++ scripts/script.js
@@ -3,32 +3,31 @@ let numberButtons = document.getElementsByClassName('number');
 let operatorButtons = document.getElementsByClassName('operator');
 let equalsButton = document.getElementById('equals');
 let clearButton = document.getElementById('clear');
-let lastOperand = document.getElementById('last-operand');
-let currentOperand = document.getElementById('current-operand');
+let lastOperandElement = document.getElementById('last-operand');
+let currentOperandElement = document.getElementById('current-operand');
+let currentOperand; // stores value of current operand
+let lastOperand; // stores value of last operand
 
 // add listeners to number keys
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener("click", function() {
         let buttonValue = this.textContent;
-        if (buttonValue) {
-            if (currentOperand.innerText === "0") {
-                currentOperand.innerText = buttonValue;
-            } else {
-                currentOperand.innerText = (currentOperand.innerText + buttonValue).substring(0, 10);
-            }
+        if (currentOperandElement.innerText === "0") {
+            currentOperandElement.innerText = buttonValue;
+        } else {
+            currentOperandElement.innerText = (currentOperandElement.innerText + buttonValue).substring(0, 10);
         }
     });
 }
 
 function clearAll() {
-    currentOperand.innerText = "0";
+    currentOperandElement.innerText = "0";
 };
 
 function add() {
-    let num1 = Number(currentOperand.innerText);
-    storeLastOperand(num1);
-}
 
-function storeLastOperand(n) {
-    console.log(n);
-}
+};
+
+function operate() {
+
+};
