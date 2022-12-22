bb6b20ca0632d77b5f9dbc3b037da3fe46c17132
jeffbai996 <j.bai@wustl.edu>
2022-12-22 10:12:33 -0800

    Refactored script file for more modularity, implemented multiple clicks of equals operator

--- scripts/script.js
+++ scripts/script.js
@@ -1,106 +1,108 @@
 // DOM elements
-const numberButtons = document.querySelectorAll('.number');
-const operatorButtons = document.querySelectorAll('.operator');
-const equalsButton = document.querySelector('#equals');
-const clearButton = document.querySelector('#clear');
-const deleteButton = document.querySelector('#delete');
 const displayValue = document.querySelector('#display-value');
 
-// stored data
+// Event listeners
+document.querySelectorAll('.number').forEach((number) => {
+    number.addEventListener('click', () => handleNumberClick(number));
+});
+
+document.querySelector('#delete').addEventListener('click', handleDeleteClick);
+
+document.querySelectorAll('.operator').forEach((operator) => {
+    operator.addEventListener('click', () => handleOperatorClick(operator));
+});
+
+document.querySelector('#equals').addEventListener('click', handleEqualsClick);
+
+document.querySelector('#clear').addEventListener('click', clearAll);
+
+// Stored data
 let currentNumber = '';
 let storedNumber = '';
 let clickedOperator = '';
 let result = '';
 let lastOperand = '';
+let lastOperator = '';
 
 displayValue.innerText = 0;
 
 // Calculation functions
 function operate(a, b, operator) {
-    switch(operator) {
-        case "+":
-            return a + b;
-        case "-":
-            return a - b;
-        case "÷":
-            if (b === 0) {
-                return NaN;
-            } else {
-                return a / b;
-            }
-        case "×":
-            return a * b;
-    }
+    const operations = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '÷': (a, b) => (b === 0 ? NaN : a / b),
+        '×': (a, b) => a * b,
+    };
+    return operations[operator](a, b);
 };
 
-// DOM manipulation and user input functions
-numberButtons.forEach((number) => {
-    number.addEventListener('click', function() {
-        if (storedNumber.length < 11) {
-            storedNumber += number.innerText;
-            displayValue.innerText = storedNumber;
-        }
-    })
-}); 
+function updateDisplay(value) {
+    displayValue.textContent = value.toString().length > 11 ? value.toExponential(5) : value;
+}
 
-deleteButton.addEventListener('click', function() {
+function handleNumberClick(number) {
+    if (storedNumber.length < 11) {
+        storedNumber += number.innerText;
+        updateDisplay(storedNumber);
+    }
+}
+
+function handleDeleteClick() {
     if (storedNumber.length > 1) {
         storedNumber = storedNumber.slice(0, -1);
-        displayValue.innerText = storedNumber;
     } else {
         storedNumber = 0;
-        displayValue.innerText = "0";
     }
-});
+    updateDisplay(storedNumber);
+}
 
-operatorButtons.forEach((operator) => {
-    operator.addEventListener('click', function() {
-        // handle stringing operations
-        if (currentNumber !== 0 && storedNumber !== 0) {
-            displayResult();
-        }
-        // store the number/operator inputted
-        currentNumber = storedNumber;
-        clickedOperator = operator.innerText;
-        // displays current number + selected operator
-        if (currentNumber.toString().length > 11) {
-            displayValue.textContent = `${currentNumber.toExponential(fractionDigits = 5)} ${clickedOperator}`;
-        } else {
-            displayValue.textContent = `${currentNumber} ${clickedOperator}`;
-        }
-        // clear stored number for next calculation
-        storedNumber = '';
-        // reset last operand
-        lastOperand = '';
-    })
-});
+function handleOperatorClick(operator) {
+    if (currentNumber !== 0 && storedNumber !== 0) {
+        displayResult();
+    }
+    currentNumber = storedNumber;
+    clickedOperator = operator.innerText;
+    updateDisplay(`${currentNumber} ${clickedOperator}`);
+    storedNumber = '';
+}
 
-equalsButton.addEventListener('click', function() {
-    displayResult();
-});
+function handleEqualsClick() {
+    if (currentNumber && storedNumber) {
+        displayResult();
+        lastOperator = clickedOperator;
+        lastOperand = storedNumber;
+    } else if (currentNumber && lastOperand) {
+        result = operate(Number(currentNumber), Number(lastOperand), lastOperator);
+        updateDisplay(result);
+        currentNumber = result;
+    }
+    storedNumber = '';
+}
 
 function displayResult() {
-    // if storedNumber is null, use last operand for repeated operation
-    let currentOperand = storedNumber ? storedNumber : lastOperand;
-    // check if there are two values to operate on
-    if (currentNumber && currentOperand) {
-        result = operate(Number(currentNumber), Number(currentOperand), clickedOperator);
-        if (result.toString().length > 11) { 
-            displayValue.textContent = result.toExponential(fractionDigits = 5);
-        } else {
-            displayValue.textContent = result;
-        }
-        // store the result
-        storedNumber = result;
-        // update lastOperand with current operand
-        lastOperand = currentOperand;
-        // reset the current number
-        currentNumber = 0;
-    }
-};
+    if (currentNumber && storedNumber) {
+        result = operate(Number(currentNumber), Number(storedNumber), clickedOperator);
+        updateDisplay(result);
+        currentNumber = result;
+    }// Event listeners
+document.querySelectorAll('.number').forEach((number) => {
+    number.addEventListener('click', () => handleNumberClick(number));
+});
+
+document.querySelector('#delete').addEventListener('click', handleDeleteClick);
+
+document.querySelectorAll('.operator').forEach((operator) => {
+    operator.addEventListener('click', () => handleOperatorClick(operator));
+});
+
+document.querySelector('#equals').addEventListener('click', handleEqualsClick);
+
+document.querySelector('#clear').addEventListener('click', clearAll);
+}
 
 function clearAll() {
-    displayValue.innerText = "0";
+    updateDisplay(0);
     currentNumber = '';
     storedNumber = '';
-};
+}
